71d75bb696e57a61f4c4f6f915bd3bdc17d7e2c8
djsc6511 <djsc6511@naver.com>
2017-02-13 22:23:07 +0900

    create package

--- app/src/main/java/kr/com/peoch/peoch_android/view/MainActivity.java
+++ app/src/main/java/kr/com/peoch/peoch_android/view/MainActivity.java
@@ -1,7 +1,9 @@
-package kr.com.peoch.peoch_android;
+package kr.com.peoch.peoch_android.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kr.com.peoch.peoch_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
